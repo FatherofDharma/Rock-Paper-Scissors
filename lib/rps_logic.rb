cd39c7c16adbb6e708459079254983dd6ad59d2c
Daniel Hongel and Gavin Kimberlin <gavin@>
2019-07-17 11:25:37 -0700

    add conditions for each selection to lose as well as return win or lose statements to the user.

--- lib/rps_logic.rb
+++ lib/rps_logic.rb
@@ -1,20 +1,46 @@
+#!/usr/bin/ruby
+
 require('pry')
+
+
 class RPS
   def initialize()
 
+
+
   end
   def wins?(object, argument)
+    print "Choose rock, paper, or scissors: "
+    @input = (gets.chomp).downcase
     @object = object
     @argument = argument
-    if @object == "rock" && @argument == "scissors"
-       true
-    elsif @object == "paper" && @argument == "rock"
+    win = "#{@input} Wins!"
+    lose = "#{@input} Loses!"
+    if @input == "rock" && @argument == "scissors"
+      puts win
+     true
+    elsif @input == "rock" && @argument == "paper"
+      puts lose
+      false
+    elsif @input == "paper" && @argument == "rock"
+      puts win
       true
-    elsif @object == "scissors" && @argument == "paper"
+    elsif @input == "paper" && @argument == "scissors"
+      puts lose
+      false
+    elsif @input == "scissors" && @argument == "paper"
+      puts win
       true
-    elsif @object == @argument
+    elsif @input == "scissors" && @argument == "rock"
+      puts lose
+      false
+    elsif @input == @argument
+      puts "It's a tie!"
       false
     end
 
   end
 end
+
+game = RPS.new()
+game.wins?("scissors", "rock")
