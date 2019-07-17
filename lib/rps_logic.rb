1fcad112a20762bb0d04168006fc3ef52930fb09
Daniel Hongel and Gavin Kimberlin <daniel+gavin@>
2019-07-17 10:47:47 -0700

    add specs for our rps game. first four are passing.

--- lib/rps_logic.rb
+++ lib/rps_logic.rb
@@ -1,1 +1,20 @@
 require('pry')
+class RPS
+  def initialize()
+
+  end
+  def wins?(object, argument)
+    @object = object
+    @argument = argument
+    if @object == "rock" && @argument == "scissors"
+       true
+    elsif @object == "paper" && @argument == "rock"
+      true
+    elsif @object == "scissors" && @argument == "paper"
+      true
+    elsif @object == @argument
+      false
+    end
+
+  end
+end
--- spec/rps_logic_spec.rb
+++ spec/rps_logic_spec.rb
@@ -1,2 +1,24 @@
 require('rspec')
 require('rps_logic')
+
+describe('RPS') do
+  it("returns true if rock is the object and scissors is the argument") do
+    game = RPS.new()
+    expect(game.wins?("rock", "scissors")).to(eq(true))
+  end
+
+  it("returns true if paper is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("paper", "rock")).to(eq(true))
+  end
+
+    it("returns true if scissors is the object and paper is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "paper")).to(eq(true))
+  end
+
+  it("returns false if the object is equal to the argument") do
+  game = RPS.new()
+  expect(game.wins?("scissors", "scissors")).to(eq(false))
+  end
+end
